024905475200c6349bc4c28b6e4c574aabf8d315
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-14 10:45:50 -0800

    Initialization of files

---
+++ OOP_Principles_Zoo/OOP_Principles_Zoo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OOP_Principles_Zoo.Classes;
+
+namespace OOP_Principles_Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ OOP_Principles_Zoo/ZooUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using OOP_Principles_Zoo.Classes;
+
+namespace ZooUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
