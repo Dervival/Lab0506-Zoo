8eaa786e712d1302631fcedef728bf433688401a
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-15 11:16:01 -0800

    Added system design image with interfaces, declared IFly and IHibernate interfaces

---
+++ OOP_Principles_Zoo/OOP_Principles_Zoo/Interfaces/IFly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Principles_Zoo.Interfaces
+{
+    public interface IFly
+    {
+        bool IsFlying { get; set; }
+        string TakeOff();
+        string Land(string location);
+    }
+}
