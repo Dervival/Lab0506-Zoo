bd56a0640d211d586c7ca91b6f810fb81acbcc58
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-15 09:33:58 -0800

    Added reptile class

--- OOP_Principles_Zoo/OOP_Principles_Zoo/Classes/Reptile.cs
+++ OOP_Principles_Zoo/OOP_Principles_Zoo/Classes/Reptile.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace OOP_Principles_Zoo.Classes
 {
-    class Reptile : Animal
+    abstract class Reptile : Animal
     {
+        public virtual bool BreathesFire { get; set; } = false;
+
+        public override string Sleep()
+        {
+            string sleepMessage = "The " + AnimalName + " sleeps in its den.";
+            Console.WriteLine(sleepMessage);
+            return sleepMessage;
+        }
+
+        public string Firebreath()
+        {
+            if (BreathesFire)
+            {
+                string fireMessage = "The " + AnimalName + " breathes fire at you!";
+                Console.WriteLine(fireMessage);
+                return fireMessage;
+            }
+            else
+            {
+                string fireMessage = "The " + AnimalName + " looks sadly at you, as it cannot breathe fire.";
+                Console.WriteLine(fireMessage);
+                return fireMessage;
+            }
+        }
     }
 }
