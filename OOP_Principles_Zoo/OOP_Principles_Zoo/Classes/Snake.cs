1d908a73f67faa5a224337b3d8ebda1723b3d7a6
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-15 09:43:00 -0800

    Added snake class

--- OOP_Principles_Zoo/OOP_Principles_Zoo/Classes/Snake.cs
+++ OOP_Principles_Zoo/OOP_Principles_Zoo/Classes/Snake.cs
@@ -6,5 +6,29 @@ namespace OOP_Principles_Zoo.Classes
 {
     class Snake : Reptile
     {
+        public override string AnimalName { get; set; } = "Snake";
+        public override int NumberOfLegs { get; set; } = 0;
+        public override bool IsReal { get; set; } = true;
+
+        public override string Eat()
+        {
+            string eatMessage = "The " + AnimalName + " is eating a mouse.";
+            Console.WriteLine(eatMessage);
+            return eatMessage;
+        }
+
+        public override string Sleep()
+        {
+            string sleepMessage = "During the winter, " + base.Sleep().ToLower();
+            Console.WriteLine(sleepMessage);
+            return sleepMessage;
+        }
+
+        public override string Walk()
+        {
+            string walkMessage = "As the snake has no legs, it cannot walk.";
+            Console.WriteLine(walkMessage);
+            return walkMessage;
+        }
     }
 }
